88666900998d6b82873f642db946cce588c87fef
Thom Allen <thomwork@gmail.com>
2015-12-07 23:55:56 +0000

    Add bootstrap, debug mode on

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,22 @@
+Vue.config.debug = true;
 
-// Model
-var exampleData = {
-		newTodo: '',
-		todos: [
-		]
-};
-
-// ViewModel
 new Vue({
-	el: '#app',
-	data: exampleData,
-	methods: {
-		addTodo: function() {
-			var text = this.newTodo.trim();
-			if (text) {
-				this.todos.push({text:text});
-				this.newTodo = '';
-			}
-		},
-		removeTodo: function(index) {
-			this.todos.splice(index, 1);
-		}
-	}
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: [
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo.trim();
+      if (text) {
+        this.todos.push({ text: text });
+        this.newTodo = '';
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1);
+    }
+  }
 });
-
-//# sourceMappingURL=main.js.map
