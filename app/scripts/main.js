ed90d5dafa1b3b26a90686e78ba20ac8d105fc43
Thom Allen <thomwork@gmail.com>
2015-12-09 15:08:17 +0000

    Basic edit/completed toggling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,26 @@ var vm = new Vue({
 	el: '#app',
 	data: {
 		newTask: '',
+		editedTask: '',
 		tasks: [
-			{ text: "A task", completed: false },
-			{ text: "A task", completed: false },
-			{ text: "A completed task", completed: true } 
-		],
+			{ text: "A task", completed: false, editing: false },
+			{ text: "A task", completed: false, editing: false },
+			{ text: "A completed task", completed: true, editing: false } 
+		]
+	},
+	methods: {
+		addTask: function() {
+			var text = this.newTask.trim();
+			if ( text ) {
+				this.tasks.push( { text:text, completed: false } );
+				this.newTask = '';
+			}
+		},
+		toggleEdit: function( task ) {
+			task.editing = ! task.editing;
+		},
+		toggleCompleted: function ( task ) {
+			task.completed = ! task.completed;
+		}
 	}
 });
