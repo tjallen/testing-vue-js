56ca619ada988ab95b601057783e13fc5024b42b
Thom Allen <thomwork@gmail.com>
2015-12-12 23:46:33 +0000

    Partly done stuff. Amend me tomorrow!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,70 @@
 Vue.config.debug = true;
 
 // extend and register a global component in 1 step
-Vue.component('my-component', {
-	template: '#my-template',
-	props: ['subject'],
-	// use return for data in components = not shared
+var VC = Vue.component('tasks-component', {
+	template: '#tasks-template',
 	data: function() {
-		return { answer: 42 };
+		return {
+			newTask: '',
+			editedTask: '',
+		};
+	},
+	props: ['list'],
+	computed: {
+		remaining: function() {
+			return this.list.filter( this.isActive ).length;
+		}
+	},
+	methods: {
+		addTask: function() {
+			var text = this.newTask.trim();
+			if ( text ) {
+				this.list.push({ text: text, completed: false, editing: false });
+				this.newTask = '';
+			}
+		},
+		deleteTask: function( task ) {
+			this.list.$remove( task );
+		},
+		isComplete: function( task ) {
+			return task.completed;
+		},
+		isActive: function ( task ) {
+			return ! this.isComplete( task );
+		},
+		toggleCompleted: function ( task ) {
+			task.completed = ! task.completed;
+		},
+		startEdit: function( task ) {
+			this.originalText = task.text;
+			this.editedTask = task;
+		},
+		completeEdit: function( task ) {
+			console.log('completedit');
+			if (this.editedTask) {
+				return;
+			}
+			this.editedTask = '';
+			task.title = task.text;
+			// if text is empty, delete this task
+			if (!task.title) {
+				this.deleteTask( task );
+			}
+		},
+		cancelEdit: function( task ) {
+			task.text = this.originalText;
+			this.editedTask = '';
+		},
+		clearCompleted: function() {
+			this.list = this.list.filter(this.isActive);
+		}
 	}
 });
 
 var vm = new Vue({
 	el: '#app',
 	data: {
-		newTask: '',
-		editedTask: '',
 		tasks: [
-			{ text: "A task", completed: false, editing: false },
 			{ text: "A task", completed: false, editing: false },
 			{ text: "A completed task", completed: true, editing: false } 
 		]
